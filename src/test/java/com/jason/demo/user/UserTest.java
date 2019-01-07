67c9b8b96850c72efc1adfc5fe68b81b6dc27104
jason <jason@seafarer.me>
2019-01-08 00:00:36 +0800

    使用map查询

--- src/test/java/com/jason/demo/user/UserTest.java
+++ src/test/java/com/jason/demo/user/UserTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,7 +25,12 @@ public class UserTest {
         System.out.println(("----- selectAll method test ----"));
 //        List<User> userList = userMapper.selectList(null);
 
-        List<User> userList = userMapper.selectMethod();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Adriel West");
+        map.put("age", 0);
+
+        List<User> userList = userMapper.selectByMap(map);
         System.out.println(userList);
 
 //        Assert.assertEquals(5, userList.size());
