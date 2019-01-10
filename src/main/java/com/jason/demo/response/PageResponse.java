82dad87d60374107ce770e281f8044c3c7909ba8
jason <jason@seafarer.me>
2019-01-10 22:27:41 +0800

    增加swagger注解
    Signed-off-by: jason <jason@seafarer.me>

--- src/main/java/com/jason/demo/response/PageResponse.java
+++ src/main/java/com/jason/demo/response/PageResponse.java
@@ -1,15 +1,24 @@
 package com.jason.demo.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import java.util.List;
 
 @Data
 public class PageResponse<T> {
 
+    @ApiModelProperty(value = "总数目")
     private int total;
+
+    @ApiModelProperty(value = "单页数目")
     private int size;
+
+    @ApiModelProperty(value = "页数")
     private int pages;
+
+    @ApiModelProperty(value = "当前页")
     private int current;
 
-    private List<T> data;
+    @ApiModelProperty(value = "列表记录")
+    private List<T> records;
 }
