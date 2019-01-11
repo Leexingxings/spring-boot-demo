16e3771d2a90b2b13a81642a7b3c2939de61746c
jason <jason@seafarer.me>
2019-01-11 22:01:02 +0800

    @Valid验证不通过返回第一个报错信息
    Signed-off-by: jason <jason@seafarer.me>

--- src/main/java/com/jason/demo/controller/BaseController.java
+++ src/main/java/com/jason/demo/controller/BaseController.java
@@ -3,7 +3,6 @@ package com.jason.demo.controller;
 import com.jason.demo.base.ResponseData;
 import com.jason.demo.constants.ResponseCode;
 import com.jason.demo.utils.ResponseUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.BindingResult;
 
 /**
@@ -16,17 +15,20 @@ public class BaseController {
     /**
      * 获取错误响应
      *
-     * @param result
-     * @return
+     * @param result 报错信息
+     * @return ResponseUtil
      */
     protected ResponseData getErrorResponse(BindingResult result) {
         //请求的数据参数格式不正确
         if (result.hasErrors()) {
-            String[] errArr = new String[result.getAllErrors().size()];
-            for (int i = 0; i < result.getAllErrors().size(); i++) {
-                errArr[i] = result.getAllErrors().get(i).getDefaultMessage();
-            }
-            String errMsg = StringUtils.join(errArr, ",");
+//            String[] errArr = new String[result.getAllErrors().size()];
+//            for (int i = 0; i < result.getAllErrors().size(); i++) {
+//                errArr[i] = result.getAllErrors().get(i).getDefaultMessage();
+//            }
+//            String errMsg = StringUtils.join(errArr, ",");
+
+            // 获取第一个报错信息
+            String errMsg = result.getAllErrors().get(0).getDefaultMessage();
 
             return ResponseUtil.build(ResponseCode.PARAM_INVALID.getValue(), errMsg);
         }
